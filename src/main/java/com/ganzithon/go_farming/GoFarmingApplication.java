7639baac4de971d55f0dfd442300efb85e40fc2a
Zena0128 <0128ja@gmail.com>
2024-11-12 17:49:31 +0900

    [INIT] 프로젝트 시작

---
+++ src/main/java/com/ganzithon/go_farming/GoFarmingApplication.java
@@ -0,0 +1,13 @@
+package com.ganzithon.go_farming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoFarmingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganzithon/go_farming/GoFarmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganzithon.go_farming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoFarmingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
