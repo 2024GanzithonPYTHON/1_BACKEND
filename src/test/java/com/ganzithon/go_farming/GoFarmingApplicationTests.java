75d56d4345a37224df4422ddec9d8099d8551b8a
Zena0128 <0128ja@gmail.com>
2024-11-18 20:51:00 +0900

    [INFRA] 어떤 yml 사용할지 설정

--- src/test/java/com/ganzithon/go_farming/GoFarmingApplicationTests.java
+++ src/test/java/com/ganzithon/go_farming/GoFarmingApplicationTests.java
@@ -2,8 +2,10 @@ package com.ganzithon.go_farming;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GoFarmingApplicationTests {
 
 	@Test
